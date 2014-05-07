ed5e27cbeb5ec108b8f536de1754ae82cd215dca
Jason <jaytomsig@gmail.com>
2014-05-07 22:07:28 +0800

    removed fb login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
  protect_from_forgery with: :exception
  before_filter :configure_permitted_parameters, if: :devise_controller?
 
- private
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+ # private
+ #  def current_user
+ #    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+ #  end
+ #  helper_method :current_user
   
 protected
 
